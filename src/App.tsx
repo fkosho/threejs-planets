af9947b6bdb45fb1d9c99412cca78a0ce0fec6c3
fkosho <fkdev815@gmail.com>
2022-05-15 23:28:26 +0900

    add a discription modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { useEffect } from "react"
+import { DescriptionModal } from "./components/molecules/DescriptionModal"
 import { useMoveCube } from "./hooks/use3dActions/useMoveCube"
 
 export default function App() {
 
     return (
-        <div 
-            id='sample'
-            onClick={() => useMoveCube()}
-            style={{ 
-                color: 'white', 
-                backgroundColor: '#345678',
-                width: '100px',
-                height: '50px',
-                borderRadius: '30px'
-                
-            }}></div>
+        <>
+            <div
+                id='sample'
+                onClick={() => useMoveCube()}
+                style={{
+                    color: 'white',
+                    backgroundColor: '#345678',
+                    width: '100px',
+                    height: '50px',
+                    borderRadius: '30px'
+
+                }}
+            >
+
+            </div>
+            <DescriptionModal />
+        </>
     )
 }
---
+++ src/components/molecules/DescriptionModal.tsx
@@ -0,0 +1,12 @@
+export const DescriptionModal = () => {
+    return (
+        <div style={{
+            borderRadius: '20px',
+            height: '200px',
+            width: '100px',
+            backgroundColor: '#783423'
+        }}>
+
+        </div>
+    )
+}
