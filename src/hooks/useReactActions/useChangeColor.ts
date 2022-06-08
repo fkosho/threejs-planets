52f0774f31a340bf4dad80747a5fafa2c50f074a
AQ-Kyosuke-Kaneko <kyosuke.kaneko@asia-quest.jp>
2022-06-08 18:57:30 +0900

    [change]var2let

--- src/hooks/useReactActions/useChangeColor.ts
+++ src/hooks/useReactActions/useChangeColor.ts
@@ -1,6 +1,6 @@
 export const useChangeColor = (e: React.MouseEvent<HTMLElement>) => {
   var randomColor = "#";
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     randomColor += ((16 * Math.random()) | 0).toString(16);
   }
   e.currentTarget.style.backgroundColor = randomColor;
